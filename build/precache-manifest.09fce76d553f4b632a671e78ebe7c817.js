6ff5a5cc60caf1028cbb90ee229f806d5c22a9a6
Jawad Shaikh <shaikhjawad007@gmail.com>
2018-10-16 14:34:44 +0530

    updating config for build

---
+++ build/precache-manifest.09fce76d553f4b632a671e78ebe7c817.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a54e7fbf41c70e3d169",
+    "url": "/react-material-calculator/static/js/runtime~main.6a54e7fb.js"
+  },
+  {
+    "revision": "8a2d82954219a21b37a6",
+    "url": "/react-material-calculator/static/js/main.8a2d8295.chunk.js"
+  },
+  {
+    "revision": "ac0918a55aaee4fd9b55",
+    "url": "/react-material-calculator/static/js/1.ac0918a5.chunk.js"
+  },
+  {
+    "revision": "8a2d82954219a21b37a6",
+    "url": "/react-material-calculator/static/css/main.3f83b9c1.chunk.css"
+  },
+  {
+    "revision": "6ed967f2b707d49bcc3a307eb89b4eed",
+    "url": "/react-material-calculator/index.html"
+  }
+];
